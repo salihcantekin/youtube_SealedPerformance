0cc83d77015af465f4d5b467b159239b370280f0
Salih Cantekin <salihcantekin@gmail.com>
2023-05-02 22:21:04 +0100

    Add project files.

---
+++ SealedPerformance/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<BenchmarkTest>();
+
+public class BenchmarkTest
+{
+    private readonly NonSealedClass _nonSealedInstance = new();
+    private readonly SealedClass _sealedInstance = new();
+
+    [Benchmark]
+    public int NonSealedClassBenchmark()
+    {
+        return _nonSealedInstance.Calculate(5, 5);
+    }
+
+    [Benchmark]
+    public int SealedClassBenchmark()
+    {
+        return _sealedInstance.Calculate(5, 5);
+    }
+}
+
+
+
+// devirtualization
+
+public sealed class SealedClass
+{
+    public int Calculate(int x, int y)
+    {
+        return x * y;
+    }
+}
+
+public class NonSealedClass
+{
+    public virtual int Calculate(int x, int y)
+    {
+        return x + y;
+    }
+}
